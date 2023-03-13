87254df307ac9389ff0a8bae205bf1cf2d52b638
Shachak Armon <shachak.armon@gmail.com>
2023-03-13 18:06:08 +0200

    header styling

--- src/cmps/Section.js
+++ src/cmps/Section.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+// import { Fade } from 'react-reveal/Fade'
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{title}</h1>
-                <p> {description} </p>
-            </ItemText>
+            
+            {/* <Fade bottom> */}
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p> {description} </p>
+                </ItemText>
+            {/* </Fade> */}
 
             <Buttons>
                 <ButtonGroup>
