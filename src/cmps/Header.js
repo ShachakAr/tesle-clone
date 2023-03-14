309c0b140ae357947a7f2617389955b59c7334fe
Shachak Armon <shachak.armon@gmail.com>
2023-03-14 09:50:26 +0200

    header navbar

--- src/cmps/Header.js
+++ src/cmps/Header.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+
 
 function Header() {
+ 
+    const [burgerStatus, setBurgerStatus] = useState(false)
+
     return (
         <Container>
             <a>
@@ -17,8 +22,20 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={() => setBurgerStatus(true)} />
             </RightMenu>
+
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setBurgerStatus(false)}/>
+                </CloseWrapper>
+                <li><a href='#'>Existing Inventory</a></li>
+                <li><a href='#'>Used Inventory</a></li>
+                <li><a href='#'>Trade-in</a></li>
+                <li><a href='#'>Cybertruck</a></li>
+                <li><a href='#'>Roadster</a></li>
+            </BurgerNav>
+
         </Container>
     )
 }
@@ -35,6 +52,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 10;
 `
 
 const Menu = styled.div`
@@ -66,8 +84,40 @@ const RightMenu = styled.div`
         margin-right: 10px;
     }
     
+    `
+    
+    const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
 `
 
-const CustomMenu = styled(MenuIcon)`
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background-color: white;
+    width: 300px;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s ease-in;
+    li {
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+        a {
+            font-weight: 600;
+        }
+    }    
+`
+const CloseWrapper = styled.div`
+ display: flex;
+ justify-content: flex-end;
+`
+
+const CustomClose = styled(CloseIcon)`
     cursor: pointer;
 `
--- src/cmps/Section.js
+++ src/cmps/Section.js
@@ -1,31 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-// import { Fade } from 'react-reveal/Fade'
+import React from 'react';
+import styled from 'styled-components';
+import Fade from 'react-reveal';
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
         <Wrap bgImage={backgroundImg}>
-            
-            {/* <Fade bottom> */}
+
+            <Fade bottom>
                 <ItemText>
                     <h1>{title}</h1>
                     <p> {description} </p>
                 </ItemText>
-            {/* </Fade> */}
+            </Fade>
 
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        {leftBtnText}
-                    </LeftButton>
-                    {rightBtnText &&
-                        <RightButton>
-                            {rightBtnText}
-                        </RightButton>
-                    }
-                </ButtonGroup>
-
+                <Fade buttom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {rightBtnText &&
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src="/images/down-arrow.svg" />
+
             </Buttons>
         </Wrap>
     )
